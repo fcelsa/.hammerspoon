fe7eed7b14057dcc88432c78d1bcbd3066030858
Fabio Celsalonga <fabio.celsa@gmail.com>
2025-02-17 11:25:00 +0100

    Added gitignore and fix for code documentation

--- codedocfix.lua
+++ codedocfix.lua
@@ -1,3 +1,6 @@
 -- global hammerspoon object (workaround for code documentation help - sumneko Lua extension)
-hs = (...)
-spoon = (...)
+-- hs = (...)
+-- spoon = (...)
+
+_G.hs = _G.hs or {}
+_G.spoon = _G.spoon or {}
