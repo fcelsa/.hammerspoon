60ecfe9b2fa7a5b965c55bd7986a58d00aba30b3
Fabio Celsalonga <fabio.celsa@gmail.com>
2025-02-17 19:57:59 +0100

    fixed some warning issue and the bug when Terminal not being open, improved documentation comments.

--- drag_term.lua
+++ drag_term.lua
@@ -1,14 +1,23 @@
--- drag_term.lua
--- draw a rectangle on screen with ⌥+shift to put Terminal in that position and dimension.
--- If terminal.app it's closed will be open; if more than terminal window it's open, will take the first one found.
--- Fabio Celsalonga 10/03/2020
--- TODO: hs.eventtap.new dovrebbe essere una global e non local, altrimenti dopo un po' di tempo non funziona più.
---       questo a causa della garbage collection del lua che "scarica" dalla memoria le local dopo un po' di tempo.
--- fix anche della dimensione del rettangolo, altrimenti bastava premere solo ctrl+shift per triggerare l'apertura
--- ora deve essere selezionato un rettangolo più grande di 640x480
+--- drag_term.lua
+--- hold down ⌥+⇧ (alt+shift) and drag mouse on screen draw a rectangle preview
+--- wich then put Terminal app in that position and dimension;
+--- rectangle needs to be at least 320x240 to trigger the opening of Terminal;
+--- if Terminal.app it's closed will be open;
+--- if more than Terminal window it's open, will take the last one had focus,
+--- regardless if it is not in the foreground;
+--- only unique or last one minimized windows are restored,
+--- it brings it to the foreground and resizes it.
+--- Works also on multi-screen setups.
+--- Implementation note:
+--- hs.eventtap.new should be a global variable and not local, otherwise,
+--- after some time, it stops working due to Lua's garbage collection which
+--- "unloads" local variables from memory after a while.
+--- Fabio Celsalonga 1st implementation 10/03/2020 last update 17/02/2025
 
 local color = { red = 192 / 255, green = 192 / 255, blue = 220 / 255, alpha = 0.3 }
 local strokeColor = { red = 0 / 255, green = 100 / 255, blue = 255 / 255, alpha = 1 }
+
+local fromPoint = nil
 
 local rectanglePreview = hs.drawing.rectangle(hs.geometry.rect(0, 0, 0, 0))
 rectanglePreview:setStrokeWidth(4)
@@ -18,38 +27,42 @@ rectanglePreview:setRoundedRectRadii(2, 2)
 rectanglePreview:setStroke(true):setFill(true)
 rectanglePreview:setLevel("floating")
 
-
 local function OpenTerminal()
     local frame = rectanglePreview:frame()
-    --local createTermWithBounds = string.format([[
-    --  if application /"Terminal" is not running then
-    --    launch application "Terminal"
-    --  end if
-    --  tell application "Terminal"
-    --    set newWindow to (create window with default profile)
-    --    set the bounds of newWindow to {%i, %i, %i, %i}
-    --  end tell
-    --]], frame.x, frame.y, frame.x + frame.w, frame.y + frame.h)
-    -- print("x: " .. frame.x .. "  y:" .. frame.y .. "  w:" .. frame.w .. "  h:" .. frame.h)
-    if frame.w >= 640 and frame.h >= 480 then
-        local termFocused = hs.application.launchOrFocus("Terminal")
-        if termFocused then
-            local win = hs.window.focusedWindow()
+    local termFocused = false
+    local termAppRunning = nil
+    local win = nil
+    if frame.w >= 320 and frame.h >= 240 then
+        termAppRunning = hs.application.get("com.apple.Terminal")
+        termFocused = hs.application.launchOrFocus("Terminal")
+        if termAppRunning ~= nil and termFocused then
+            win = hs.window.focusedWindow()
             local f = win:frame()
             f.x = frame.x
             f.y = frame.y
             f.w = frame.w
             f.h = frame.h
             win:setFrame(f)
+        elseif termAppRunning == nil and termFocused then  -- I'm sure that can be done better
+            hs.timer.doAfter(1, function()
+                win = hs.window.focusedWindow()
+                local f = win:frame()
+                f.x = frame.x
+                f.y = frame.y
+                f.w = frame.w
+                f.h = frame.h
+                win:setFrame(f)
+            end)
         end
     end
 end
 
-local fromPoint = nil
-
 DragEvent = hs.eventtap.new(
     { hs.eventtap.event.types.mouseMoved },
     function(e)
+        if fromPoint == nil then
+            return nil
+        end
         local toPoint = hs.mouse.absolutePosition()
         local newFrame = hs.geometry.new({
             x1 = fromPoint.x,
@@ -58,7 +71,6 @@ DragEvent = hs.eventtap.new(
             y2 = toPoint.y,
         })
         rectanglePreview:setFrame(newFrame)
-
         return nil
     end
 )
