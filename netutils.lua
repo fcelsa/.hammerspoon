59888054d98a8c09cd0dabd36232d58d83b883b2
fcelsa <fabio.celsa@gmail.com>
2025-12-19 08:33:31 +0100

    added functions to netutils

--- netutils.lua
+++ netutils.lua
@@ -1,3 +1,6 @@
+-- netutils: module for network connection utility
+
+-- PingResult function test network quality and show alert with result.
 function PingResult(object, message, seqnum, error)
     local avg
 
@@ -15,4 +18,27 @@ function PingResult(object, message, seqnum, error)
     end
 end
 
+-- NetworkName function to show alert with all network names of the system.
+function NetworkName(networks)
+    local networksName = ""
+    if networks then
+        for _, network in pairs(networks) do
+            networksName = networksName .. network .. "\n"
+        end
+        hs.alert.show("Networks:\n" .. networksName, 10)
+    end
+end
+
+-- NetworkWifi function to show alert with current wifi network name.
+function NetworkWifi()
+    local wifi = hs.wifi.currentNetwork()
+    if wifi then
+        hs.alert.show("WiFi Network: " .. wifi, 5)
+    else
+        hs.alert.show("No WiFi Network", 5)
+    end
+end
+
 hs.hotkey.bind(Hyper, "p", function() hs.network.ping.ping("8.8.8.8", 1, 0.01, 1.0, "any", PingResult) end)
+hs.hotkey.bind(Hyper, "n", function() NetworkName(hs.host.addresses()) end)
+hs.hotkey.bind(Hyper, "w", function() NetworkWifi() end)
