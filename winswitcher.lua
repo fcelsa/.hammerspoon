3adfa59e36c300fa929dc6f54f3fd356b330d145
fcelsa <fabio.celsa@gmail.com>
2026-05-14 09:42:47 +0200

    Refactor winswitcher.lua to improve window switching functionality and reduce memory usage

--- winswitcher.lua
+++ winswitcher.lua
@@ -1,37 +1,39 @@
 -- winswitcher.lua
--- manage alt+tab  alt+\  alt+esc  to switch window with hs.window.switcher and hs.expose
+-- manage cmd+\ and cmd+esc to switch window with hs.window.switcher
+-- take in account that hs.window.switcher is a bit of a memory hog, 
+-- so we want to keep it alive and not reinstantiate it every time. 
+-- In my tests, instantiating it on demand was causing 100MB+ memory spikes, 
+-- while keeping it alive is around 50MB.
 
--- in questa modalità è lento... e non risolve il problema di "tutte le finestre"...
--- inoltre istanziare uno switcher già di suo vale 50MB in più.
-hs.window.filter.forceRefreshOnSpaceChange = true
-Filter = hs.window.filter.new(true):setOverrideFilter{ allowTitles = 1 }
-Switcher = hs.window.switcher.new(Filter)
-Switcher.ui.showTitles = false
-Switcher.ui.showThumbnails = false
--- Switcher.ui.showSelectedThumbnail = false
--- Switcher.ui.selectedThumbnailSize = 512
-Switcher.ui.showSelectedTitle = true
-Switcher.ui.textSize = 10
-Switcher.ui.backgroundColor = { 0.2, 0.5, 0.5, 0.5 }
-hs.hotkey.bind('cmd', 'escape', 'Next window', function () Switcher:next() end)
+-- hs.window.filter.forceRefreshOnSpaceChange = true
+local AllWindowsFilter = hs.window.filter.new(true):setCurrentSpace(nil)
+local OtherSpacesFilter = hs.window.filter.new(true):setCurrentSpace(false)
 
--- Switcher_finder = hs.window.switcher.new{'Finder','ForkLift'} -- only Finder and forklift window for switch
--- Switcher_finder.ui.showTitles = false
--- Switcher_finder.ui.showThumbnails = false
+AllWindowsFilter:subscribe(hs.window.filter.windowsChanged, function () end)
+OtherSpacesFilter:subscribe(hs.window.filter.windowsChanged, function () end)
 
--- EnExpose = hs.expose.new()
--- EnExpose.ui.highlightColor = {0.8,0.5,0,0.1}
--- EnExpose.ui.backgroundColor = {0,0,0.8,0.6}
--- EnExpose.ui.otherSpacesStripBackgroundColor = {0.1,0.1,0.8,0.6}
--- EnExpose.ui.otherSpacesStripPosition = 'top'
--- EnExpose.ui.otherSpacesStripWidth = 0.3
--- EnExpose.ui.nonVisibleStripBackgroundColor = {0.03,0.1,0.15,0.6}
--- hs.hotkey.bind('cmd', 'escape', 'Enanched Expose', function() EnExpose:toggleShow() end)
+local function SwitcherUIPrefs()
+	return {
+		showTitles = false,
+		showThumbnails = false,
+		showSelectedThumbnail = true,
+		thumbnailSize = 128,
+		showSelectedTitle = true,
+		fontName = 'Menlo',
+		textSize = 6,
+		textColor = { 1, 0.95, 0.8, 1 },
+		titleBackgroundColor = { 0.1, 0.1, 0.1, 0.9 },
+	}
+end
 
--- hs.grid.setGrid'4x3'
-
--- hs.hotkey.bind('alt', 'tab', 'Next window', function() Switcher:next() end)
--- hs.hotkey.bind('alt', '\\', 'Finder window', function() Switcher_finder:next() end)
--- hs.hotkey.bind('alt', 'escape', 'Enanched Expose', function() EnExpose:toggleShow() end)
--- hs.hotkey.bind('cmd', 'escape', 'Show grid position', hs.grid.show)
--- hs.hotkey.bind(Hyper, '.', 'Show shortcut hint list', hs.hints.windowHints)
+Switcher = hs.window.switcher.new(
+	AllWindowsFilter,
+	SwitcherUIPrefs()
+)
+OtherSpacesSwitcher = hs.window.switcher.new(
+	OtherSpacesFilter,
+	SwitcherUIPrefs()
+)
+hs.hotkey.bind('cmd', '\\', function() Switcher:next() end)
+hs.hotkey.bind({'cmd', 'shift'}, '\\', function() Switcher:previous() end)
+hs.hotkey.bind('cmd', 'escape', function() OtherSpacesSwitcher:next() end)
